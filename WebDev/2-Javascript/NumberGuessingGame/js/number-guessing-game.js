9f99ec6b41f1b5840fd58acb59b63a4ba129fb9c
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-08-04 06:33:59 -0400

    number guessing game

--- WebDev/2-Javascript/NumberGuessingGame/js/number-guessing-game.js
+++ WebDev/2-Javascript/NumberGuessingGame/js/number-guessing-game.js
@@ -1,4 +1,23 @@
 function runGame(){
-    guessNumber = prompt("I am thinking of a number in the range 1 to 100. \n\n What is the number?");
     
+    machinePickedNumber = Math.floor((Math.random() * 100) + 1);
+    
+    do{
+        userGuessNumber = prompt("I am thinking of a number in the range 1 to 100. \n\n What is the number?");
+        userInputValidation(userGuessNumber);
+        
+    }while(false); 
+    
+
+}
+
+function userInputValidation(userNumber){
+
+    if (isNaN(userNumber)){
+        alert("You have not entered a number \n\n Please enter a number in the 1-100 range"); 
+    }
+   
+    if (userNumber < 0 || userNumber > 100){
+        alert("Please enter a number in 1-100 range.")
+    } 
 }
