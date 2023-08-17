b308a3d74c228575745468de2588e56cbe1ec571
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-08-17 19:22:16 -0400

    Custom JavaScript Part 2

--- FullStackProject/js/scripts.js
+++ FullStackProject/js/scripts.js
@@ -1,20 +1,19 @@
-console.log('javascript connected!');
-    
 const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 2000,
     pause: false
 })
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
 })
