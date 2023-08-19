9b7cfafb2ec99da2c807c6e19777ddbe33b521dd
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-08-18 23:10:09 -0400

    shopping list exercise

--- WebDev/2-Javascript/shopping-list-project/js/scripts.js
+++ WebDev/2-Javascript/shopping-list-project/js/scripts.js
@@ -5,16 +5,22 @@ function getItem(){
     event.preventDefault();
     const shoppingListTextbox = document.getElementById('item');
     const shoppingItem = shoppingListTextbox.value;
-    if(!shoppingList.includes(shoppingItem)){
-        shoppingList.push(shoppingItem)
+    try{
+        checkInput(shoppingItem); 
+        if(!shoppingList.includes(shoppingItem)){
+            shoppingList.push(shoppingItem)
+        }
+        else{
+            alert("Item already added in the shopping list")
+        }
+        shoppingList.sort();
+        document.getElementById("myForm").reset(); 
+        removeList();
+        makeList(); 
+    }catch (error){
+        alert(error.message); 
     }
-    else{
-        alert("Item already added in the shopping list")
-    }
-    shoppingList.sort();
-    document.getElementById("myForm").reset(); 
-    removeList();
-    makeList(); 
+    
 }
 
 function makeList(){
@@ -33,5 +39,25 @@ function removeList(){
 }
 
 function removeItem(event){
+    const item = event.currentTarget.textContent;
+    const indexofItemToRemove = shoppingList.indexOf(item);
+    try{
+        if(indexofItemToRemove >= 0){
+            shoppingList.splice(indexofItemToRemove, 1); 
+            removeList();
+            makeList(); 
+        }
+        else{
+            throw new Error("The item to be removed is not available in the shopping list")
+        }
+    }catch(error){
+        alert(error.message); 
+    }
     
+}
+
+function checkInput(shoppingItem){
+    if(shoppingItem === ""){
+        throw new Error("Please enter a valid item to be added to the shopping list"); 
+    }
 }
