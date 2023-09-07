4e8eecf2abfaec10bf7b50bd1edc573e46945323
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-09-07 18:54:54 -0400

    nucampsite redux

---
+++ WebDev/3-React/nucampsite/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
