5387725e86584712feb196dd70f8c7ca7ce7bfbb
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-09-10 20:24:12 -0400

    week1 workshop

--- WebDev/3-React/nucampsite/src/features/campsites/CampsiteCard.js
+++ WebDev/3-React/nucampsite/src/features/campsites/CampsiteCard.js
@@ -1,11 +1,13 @@
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap'; 
 
-const CampsiteCard = (props) => {
+const CampsiteCard = ({campsite}) => {
+    // const {campsite} = props; //Destructuring the props
+    const { image, name } = campsite; 
     return(
         <Card>
-            <CardImg width = '100%' src = {props.campsite.image} alt = {props.campsite.name} />
+            <CardImg width = '100%' src = {image} alt = {name} />
             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     ); 
