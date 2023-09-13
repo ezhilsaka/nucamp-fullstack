c0c40d82f03fe2ec97590f2fb599a828081878af
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-09-13 18:01:50 -0400

    React Route

--- WebDev/3-React/nucampsite/src/App.js
+++ WebDev/3-React/nucampsite/src/App.js
@@ -1,4 +1,7 @@
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import CampsiteDirectoryPage from './pages/campsitesDirectoryPage';
 import Footer from './components/Footer';
 import './App.css';
@@ -7,7 +10,11 @@ function App() {
   return (
     <div className ='App'>
       <Header />
-      <CampsiteDirectoryPage />
+      <Routes>
+        <Route path ='/' element = {<HomePage />} />
+        <Route path = 'contact' element = {<ContactPage />}/>
+        <Route path = 'directory' element = {<CampsiteDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
 
