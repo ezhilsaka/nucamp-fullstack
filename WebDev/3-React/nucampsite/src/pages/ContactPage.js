37adba74f4b16a65d4f280ebb9c3217c4c882e6a
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-09-22 18:42:08 -0400

    Formik Exercises

--- WebDev/3-React/nucampsite/src/pages/ContactPage.js
+++ WebDev/3-React/nucampsite/src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import { Container, Col, Row } from "reactstrap";
 import SubHeader from "../components/SubHeader";
+import ContactForm from "../components/ContactForm";
 
 const ContactPage = () => {
     return (
@@ -40,7 +41,7 @@ const ContactPage = () => {
                     <hr />
                 </Col>
                 <Col md='10'>
-                    TBD:Contact Form
+                    <ContactForm />
                 </Col>
             </Row>
         </Container>
