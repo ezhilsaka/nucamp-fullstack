021db447d00b676636e6939d5f88abb956c7459c
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-10-04 19:53:42 -0400

    Async Thunk Partners Update

--- WebDev/3-React/nucampsite/src/App.js
+++ WebDev/3-React/nucampsite/src/App.js
@@ -10,12 +10,14 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import './App.css';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
   }, [dispatch]); 
 
   return (
