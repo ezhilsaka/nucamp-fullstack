97f87d58fe7d423cca416fe57a77683f3fa02def
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-10-18 20:07:01 -0400

    completed till campsiteinfo screen exercise

--- WebDev/4-React-Native.nosync/nucampsite/App.js
+++ WebDev/4-React-Native.nosync/nucampsite/App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./screens/MainComponent";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Mobile App World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
