da33bbd5f4b29f747716a4b5f73f0ba113937490
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-10-30 20:15:00 -0400

    week3 - exercise: Favorites

--- WebDev/4-React-Native.nosync/nucampsite/screens/MainComponent.js
+++ WebDev/4-React-Native.nosync/nucampsite/screens/MainComponent.js
@@ -16,6 +16,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
+import FavoriteScreen from './FavoriteScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -104,6 +105,29 @@ const ReservationNavigator = () => {
                     headerLeft: () => (
                         <Icon 
                             name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
+
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen 
+                name = 'Favorites'
+                component={FavoriteScreen}
+                options = {({ navigation }) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon 
+                            name='heart'
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
@@ -217,6 +241,21 @@ const Main = () => {
                     drawerIcon: ({ color }) => (
                         <Icon
                             name='tree'
+                            type='font-awesome'
+                            size={24}
+                            iconStyle={{width: 24}}
+                            color={color}
+                        />
+                    )
+                }}
+                />
+                <Drawer.Screen 
+                    name='Favorites'
+                    component={FavoritesNavigator}
+                    options={{title: 'My Favorites',
+                    drawerIcon: ({ color }) => (
+                        <Icon
+                            name='heart'
                             type='font-awesome'
                             size={24}
                             iconStyle={{width: 24}}
