57168040c364422c7b5f83e04e801917219a15e9
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-11-01 19:41:58 -0400

    week3 - exercise: gestures part 2

--- WebDev/4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.js
+++ WebDev/4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
@@ -5,11 +6,18 @@ import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
-
     const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const view = useRef();
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(1000)
+                .then((endState) => console.log(endState.finished ? 'finished': 'canceled')
+                );
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan resnponder end', gestureState);
             if (isLeftSwipe(gestureState)){
@@ -41,6 +49,7 @@ const RenderCampsite = (props) => {
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
