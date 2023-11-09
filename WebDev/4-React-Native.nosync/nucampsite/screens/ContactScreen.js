141981514319fe23e54c1abcc33c9930e3ad9033
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-11-09 16:42:44 -0500

    week4 - Exercise: Sending Email

--- WebDev/4-React-Native.nosync/nucampsite/screens/ContactScreen.js
+++ WebDev/4-React-Native.nosync/nucampsite/screens/ContactScreen.js
@@ -1,8 +1,27 @@
-import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { ScrollView, Text, Linking } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 const ContactScreen = () => {
+
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        });
+    }; 
+
+ /*    const sendMail = () => {
+        const toEmail = 'recipient@example.com';
+        const subject = 'Your email subject';
+        const body = 'Your email body';
+      
+        const mailtoLink = `mailto:${toEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      
+        Linking.openURL(mailtoLink);
+      }; */
 
     return (
         <ScrollView>
@@ -19,10 +38,23 @@ const ContactScreen = () => {
                     <Text style={{marginBottom:10}}>{"U.S.A"}</Text>
                     <Text>{"Phone: 1-206-555-1234"}</Text>
                     <Text>{"Email: campsites@nucamp.co"}</Text>
+                    <Button
+                        title='Send Email'
+                        buttonStyle={{ backgroundColor: '#5637DD', margin: 40}}
+                        icon={
+                            <Icon
+                                name='envelope-o'
+                                type="font-awesome"
+                                color='#fff'
+                                iconStyle={{ marginRight: 10}}
+                            />
+                        }
+                        onPress={() => sendMail()}
+                    />
                 </Card>
             </Animatable.View>
         </ScrollView>
-    )
+    );
 };
 
 export default ContactScreen;
