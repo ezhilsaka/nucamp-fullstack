08fbf0f4e96ee828d77c06fe16b472448f839171
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-11-11 11:43:47 -0500

    week 4 - workshop

--- WebDev/4-React-Native.nosync/nucampsite/screens/LoginScreen.js
+++ WebDev/4-React-Native.nosync/nucampsite/screens/LoginScreen.js
@@ -4,6 +4,8 @@ import { CheckBox, Input, Button, Icon } from "react-native-elements";
 import * as SecureStore from 'expo-secure-store';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 import { baseUrl } from "../shared/baseUrl";
 import logo from '../assets/images/logo.png';
 
@@ -151,9 +153,37 @@ const RegisterTab = () => {
             });
             if(capturedImage.assets){
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri); 
+                processImage(capturedImage.assets[0].uri); 
             }
         }
+    }
+
+    const getImageFromGallery = async () => {
+        const cameraPermission = 
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (cameraPermission.status === 'granted'){
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if(capturedImage.assets){
+                console.log(capturedImage.assets[0]);
+                processImage(capturedImage.assets[0].uri); 
+            }
+        }
+    }
+
+    const processImage = async (imgUri) => {
+        
+        const processedImage = await ImageManipulator.manipulateAsync(
+           imgUri,
+           [{resize:{ width: 400 }}],
+           {format: ImageManipulator.SaveFormat.PNG}
+        );
+        console.log("processed image", processedImage);
+        setImageUrl(processedImage.uri);    
+        MediaLibrary.saveToLibraryAsync(processedImage.uri); 
     }
 
     return (
@@ -166,6 +196,7 @@ const RegisterTab = () => {
                         style={styles.image}
                     />
                     <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
                 <Input
                     placeholder="Username"
--- WebDev/4-React-Native.nosync/nucampsite/screens/MainComponent.js
+++ WebDev/4-React-Native.nosync/nucampsite/screens/MainComponent.js
@@ -228,13 +228,17 @@ const Main = () => {
     }, [dispatch]);
    
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
+
+        const showNetInfo = async () => {
+            const connectionInfo = await NetInfo.fetch();
             Platform.OS === 'ios'
                 ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
                 : ToastAndroid.show(
                     'Initial Network Connectivity Type:' + connectionInfo.type, ToastAndroid.LONG
                 );
-        });
+        }
+
+        showNetInfo();
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
