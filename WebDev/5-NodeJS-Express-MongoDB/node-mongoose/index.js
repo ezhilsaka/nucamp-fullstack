abf856d686f4ba82c3d8b18fe27ea10b5846bfca
Ezhilpandiyan <ezhilpandiyan.saka@gmail.com>
2023-12-01 20:18:28 -0500

    Nodejs week2 mongoose exercises

---
+++ WebDev/5-NodeJS-Express-MongoDB/node-mongoose/index.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose'); 
+const Campsite = require('./models/campsite');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+connect.then(() => {
+    console.log('Connected correctly to server');
+
+    //const newCampsite = new Campsite({
+    Campsite.create({  
+    name: 'React Lake Campground',
+        description: 'test'
+    })
+
+    //newCampsite.save() -- not required for create method, only for new method. 
+    .then(campsite => {
+        console.log(campsite);
+
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        });
+    })
+    .then(campsite => {
+        console.log(campsite);
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save(); 
+    })
+    .then(campsite => {
+        console.log(campsite);
+        return Campsite.deleteMany();
+    })
+    .then(() => {
+        return mongoose.connection.close(); 
+    })
+    .catch(err => {
+        console.log(err);
+        mongoose.connection.close(); 
+    })
+})
